a15d09a2f3a968eba93c659cd7eb556d953b061f
Gabriele Tuccio <tuccio.gabriele@yahoo.it>
2024-07-23 21:12:02 +0200

    update

--- plot_presentation.py
+++ plot_presentation.py
@@ -10,19 +10,23 @@ data_reordered = {
 df_reordered = pd.DataFrame(data_reordered)
 
 # Plot
-plt.figure(figsize=(10, 5))
+plt.figure(figsize=(6, 5))
 plt.plot(df_reordered['Models'][1:], df_reordered['Accuracy'][1:], marker='o', linestyle='-', color='blue', label='Custom models')
 plt.axhline(y=0.47, color='red', linestyle='--', label='BERT Base')
 
 # Highlight the points
-plt.scatter(df_reordered['Models'][1], df_reordered['Accuracy'][1], color='blue', zorder=5)
-plt.scatter(df_reordered['Models'][2], df_reordered['Accuracy'][2], color='blue', zorder=5)
+plt.scatter(df_reordered['Models'][1], df_reordered['Accuracy'][1], color='blue', zorder=3)
+plt.scatter(df_reordered['Models'][2], df_reordered['Accuracy'][2], color='blue', zorder=3)
 
 # Labels and title
 plt.xlabel('Models')
 plt.ylabel('Accuracy')
 plt.title('Model Accuracy Comparison')
 plt.legend()
+
+# Set y-axis limit
+plt.ylim(0, 0.5)  # Adjust the upper limit as needed
+
 
 # Show plot
 plt.grid(True)
